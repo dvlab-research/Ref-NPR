938dfcef3599f58e18788223982a08e59a8dd4ea
Zexin He <87130754+ZexinHe@users.noreply.github.com>
2023-03-27 18:22:43 +0800

    Update env installation

--- create_env.sh
+++ create_env.sh
@@ -1,8 +1,13 @@
 conda env create -f environment.yml 
 conda activate RefNPR
-# install customized cuda kernels
+# run `python -c "import torch; print(torch.cuda.device_count())"` to examine your torch version
+# if you installed a cpu version, manually UNINSTALL and re-install certain gpu versions
+# run `conda uninstall pytorch torchvision torchaudio` then run
+# e.g. `conda install pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=11.6 -c pytorch -c conda-forge`
+
+# CUDA required here
+# install customized cuda kernels, which takes about 5 minutes to finish
+# this trick is to avoid package name confliction
 mv svox2- svox2
 python -m pip install . --upgrade --use-feature=fast-deps
 mv svox2 svox2-
-python -m pip install gdown
-# to avoid package name confliction.
